7c6aa190718f2c612250eb02b802843fbca837c0
Jason Li <jasonli2310@gmail.com>
2018-10-29 23:30:50 -0400

    update and delete

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -29,5 +29,20 @@ exports.product_details = function(req, res){
     res.send(product.name); //returns the name
   })
   
-}
+};
+
+exports.product_update = function(req, res){
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body},
+      function(err, product) {
+        if (err) return next(err);
+        res.send('Product updated.');
+      });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
 
