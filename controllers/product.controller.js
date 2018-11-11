468c925538f0fa269aa4312bb58545ff5201e234
Jason Li <jasonli2310@gmail.com>
2018-11-11 16:33:01 -0500

    client is writing to db

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -9,41 +9,36 @@ exports.test = function (req, res) {
 exports.product_create = function(req, res) {
   let product = new Product(
     {
-      // name: req.body.name,
-      // price: req.body.price
-      name: "apples",
-      price: 400
+      name: req.body.name,
+      price: req.body.price
     }
   );
 
-  product.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-    res.send('Product Created successfullyyy')
-  })
+  product.save();
+  console.log('okay ba')
+  res.send('product saved')
 };
 
 
 exports.product_details = function(req, res){
   Product.findById(req.params.id, function(err, product) {
     if (err) return next(err);
-    res.send(product); //returns the name
+    res.send(product.name); //returns the name
   })
 
 };
 
 exports.product_update = function(req, res){
-    Product.findByIdAndUpdate(req.params.id, {$set: req.body},
-      function(err, product) {
-        if (err) return next(err);
-        res.send('Product updated.');
-      });
-};
+  Product.findByIdAndUpdate(req.params.id, {$set: req.body},
+    function(err, product) {
+      if (err) return next(err);
+      res.send('Product updated.');
+    });
+  };
 
-exports.product_delete = function (req, res) {
+  exports.product_delete = function (req, res) {
     Product.findByIdAndRemove(req.params.id, function (err) {
-        if (err) return next(err);
-        res.send('Deleted successfully!');
+      if (err) return next(err);
+      res.send('Deleted successfully!');
     })
-};
+  };
